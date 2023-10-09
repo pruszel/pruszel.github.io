fdef70a9f97997200b34e5f6401533281e2d6757
Peter Ruszel <3279147+peterlopez@users.noreply.github.com>
2023-10-08 21:49:40 -0700

    clean slate

--- js/utils.js
+++
@@ -1,9 +0,0 @@
-/*
- * use this to do things after DOM has fully loaded
- */
-let ready = (callback) => {
-  if (document.readyState != "loading") callback();
-  else document.addEventListener("DOMContentLoaded", callback);
-}
-
-export { ready };
